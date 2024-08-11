e770de95c97021b5f1a980b518db56c7b5e79bc1
akshatkalra <akalra06@students.cs.ubc.ca>
2024-08-12 02:25:33 +0530

    implemented selection sort

--- src/Components/SortingVisualizer.jsx
+++ src/Components/SortingVisualizer.jsx
@@ -1,16 +1,14 @@
 import React from "react";
 import ArrayBar from "./ArrayBar";
-import {useEffect, useState } from "react";
-import './SortingVisualizer.css'
-
+import { useEffect, useState } from "react";
+import "./SortingVisualizer.css";
 
 function SortingVisualizer() {
   const [array, setArray] = useState([]);
   const [activeIndex, setActiveIndex] = useState();
   const [sortedIndices, setSortedIndices] = useState([]);
   const [sliderValue, setSliderValue] = useState(10);
-  const [speed, setSpeed] = useState('Medium');
-
+  const [speed, setSpeed] = useState("Slow");
 
   const handleSliderChange = (event) => {
     setSliderValue(event.target.value);
@@ -42,106 +40,154 @@ function SortingVisualizer() {
   const bubbleSort = async () => {
     const arr = [...array];
     const updatedIndices = [...sortedIndices];
-    const delay = speed === 'Slow' ? 800 : speed === 'Medium' ? 300 : 100;
+    const delay = speed === "Slow" ? 800 : speed === "Medium" ? 300 : 100;
 
     for (var i = 0; i < array.length; i++) {
-        for (var j = 0; j < (array.length - i - 1); j++) {
-            if(array[j] > array[j+1]) {
-                setActiveIndex(j+1);
-                var temp = array[j]
-                array[j] = array[j + 1]
-                array[j + 1] = temp
-                setArray([... array])
-                await new Promise(resolve => setTimeout(resolve, delay));
-            }
+      for (var j = 0; j < array.length - i - 1; j++) {
+        if (array[j] > array[j + 1]) {
+          setActiveIndex(j + 1);
+          var temp = array[j];
+          array[j] = array[j + 1];
+          array[j + 1] = temp;
+          setArray([...array]);
+          await new Promise((resolve) => setTimeout(resolve, delay));
         }
+      }
 
-        updatedIndices.push(arr.length - i - 1);
-        setSortedIndices([...updatedIndices]);
-
+      updatedIndices.push(arr.length - i - 1);
+      setSortedIndices([...updatedIndices]);
     }
     setActiveIndex();
-  }
+    let arr2 = [];
+    for (let i = 0; i < array.length; i++) {
+        arr2.push(i);
+        setSortedIndices([... arr2]);
+        await new Promise((resolve) => setTimeout(resolve, 50));
+    }
+  };
 
-  const insertionSort =  async () => {
-
+  const insertionSort = async () => {
     // setting the first index to sorted
     const updatedIndices = [...sortedIndices];
     updatedIndices.push(0);
     setSortedIndices([...updatedIndices]);
 
     for (let i = 1; i < array.length; i++) {
-        // starting from index 1
+      // starting from index 1
 
-        let current = array[i];
-        let j = i - 1;
-        const delay = speed === 'Slow' ? 800 : speed === 'Medium' ? 300 : 100;
+      let current = array[i];
+      let j = i - 1;
+      const delay = speed === "Slow" ? 800 : speed === "Medium" ? 300 : 100;
 
-        setActiveIndex(i);
-        await new Promise(resolve => setTimeout(resolve, delay));
+      setActiveIndex(i);
+      await new Promise((resolve) => setTimeout(resolve, delay));
 
-        while (j >= 0 && array[j] > current) {
-            setActiveIndex(j);
-            array[j+1] = array[j];
-            array[j] = current;
-            setArray([... array]);
-            await new Promise(resolve => setTimeout(resolve, delay));
-            j--;
-        }
-        // array[j + 1] = current;
-        setArray([... array]);
+      while (j >= 0 && array[j] > current) {
+        setActiveIndex(j);
+        array[j + 1] = array[j];
+        array[j] = current;
+        setArray([...array]);
+        await new Promise((resolve) => setTimeout(resolve, delay));
+        j--;
+      }
+      // array[j + 1] = current;
+      setArray([...array]);
 
-        updatedIndices.push(i);
-        setSortedIndices([...updatedIndices]);
-        await new Promise(resolve => setTimeout(resolve, delay));
+      updatedIndices.push(i);
+      setSortedIndices([...updatedIndices]);
+      await new Promise((resolve) => setTimeout(resolve, delay));
     }
-  }
+    setActiveIndex();
+    let arr = [];
+    for (let i = 0; i < array.length; i++) {
+        arr.push(i);
+        setSortedIndices([... arr]);
+        await new Promise((resolve) => setTimeout(resolve, 50));
+    }
+  };
+
+  const selectionSort = async () => {
+    const delay = speed === "Slow" ? 800 : speed === "Medium" ? 300 : 100;
+    for (let i = 0; i < array.length; i++) {
+        let minIndex = i;
+        setSortedIndices([minIndex, ...sortedIndices]);
+        await new Promise((resolve) => setTimeout(resolve, delay));
+        for (let j = i + 1; j < array.length; j++) {
+            setActiveIndex(j);
+            await new Promise((resolve) => setTimeout(resolve, delay));
+            if (array[j] < array[minIndex]) {
+                minIndex = j;
+                setSortedIndices([minIndex, ...sortedIndices]);
+                await new Promise((resolve) => setTimeout(resolve, delay));
+            }
+        }
+        var temp = array[i];
+        array[i] = array[minIndex];
+        array[minIndex] = temp;
+        setSortedIndices([i, ...sortedIndices]);
+        setArray([... array]);
+        await new Promise((resolve) => setTimeout(resolve, delay));
+        
+        
+    }
+    let arr = []
+    setActiveIndex();
+    for (let i = 0; i < array.length; i++) {
+        arr.push(i);
+        setSortedIndices([... arr]);
+        await new Promise((resolve) => setTimeout(resolve, 50));
+    }
+  };
 
   return (
     <>
-        <div className="button-container">
-            <button onClick={bubbleSort}>Bubble Sort</button>
-            <button onClick={insertionSort}>Insertion Sort</button>
-            <button onClick={generateRandomArray}>Generate Array</button>
-        </div>
-
-        <div className="array-container">
+      <div className="button-container">
+        <button onClick={bubbleSort}>Bubble Sort</button>
+        <button onClick={insertionSort}>Insertion Sort</button>
+        <button onClick={selectionSort}>Selection Sort</button>
+        <button onClick={generateRandomArray}>Generate Array</button>
+      </div>
+      <div className="array-container">
         {array.map((value, idx) => (
-            <ArrayBar key={idx} height={value} color = {idx === activeIndex ? 2 :
-                sortedIndices.includes(idx) ? 1 : 0
-            }/>
+          <ArrayBar
+            key={idx}
+            height={value}
+            color={
+              idx === activeIndex ? 2 : sortedIndices.includes(idx) ? 1 : 0
+            }
+          />
         ))}
-        </div>
+      </div>
 
-        <div className="customize-container">
-            <div>
-                <label htmlFor="arraySizeSlider">Array Size: </label>
-                <input
-                    type="range"
-                    id="arraySizeSlider"
-                    name="arraySizeSlider"
-                    min="10"
-                    max="30"
-                    value={sliderValue}
-                    onChange={handleSliderChange}
-                />
-                <span>{sliderValue}</span>
-            </div>
-            <div>
-                <label htmlFor="speedSelector">Select Speed: </label>
-                <select
-                    id="speedSelector"
-                    name="speedSelector"
-                    value={speed}
-                    onChange={handleSpeedChange}
-                >
-                    <option value="Slow">Slow</option>
-                    <option value="Medium">Medium</option>
-                    <option value="Fast">Fast</option>
-                </select>
-                <span>Selected Speed: {speed}</span>
-            </div>
+      <div className="customize-container">
+        <div>
+          <label htmlFor="arraySizeSlider">Array Size: </label>
+          <input
+            type="range"
+            id="arraySizeSlider"
+            name="arraySizeSlider"
+            min="10"
+            max="30"
+            value={sliderValue}
+            onChange={handleSliderChange}
+          />
+          <span>{sliderValue}</span>
         </div>
+        <div>
+          <label htmlFor="speedSelector">Select Speed: </label>
+          <select
+            id="speedSelector"
+            name="speedSelector"
+            value={speed}
+            onChange={handleSpeedChange}
+          >
+            <option value="Slow">Slow</option>
+            <option value="Medium">Medium</option>
+            <option value="Fast">Fast</option>
+          </select>
+          <span>Selected Speed: {speed}</span>
+        </div>
+      </div>
     </>
   );
 }
