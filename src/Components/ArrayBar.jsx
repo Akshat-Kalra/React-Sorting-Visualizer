57f26811ffaebb286211388223e81aac563bfa8f
akshatkalra <akshatkalra2005@gmail.com>
2024-08-13 15:55:46 +0530

    formatted code

--- src/Components/ArrayBar.jsx
+++ src/Components/ArrayBar.jsx
@@ -1,24 +1,26 @@
-import React from 'react'
-import './ArrayBar.css'
+import React from "react";
+import "./ArrayBar.css";
 // background-color: #61dafb;
 
 function ArrayBar(props) {
   return (
     <div
-    className='array-bar'
-    style={{
+      className="array-bar"
+      style={{
         height: `${props.height}px`,
-        backgroundColor: props.color === 0 
-        ? 'white' 
-        : props.color === 1 
-        ? 'green' 
-        : props.color === 3
-        ? 'blue'
-        : 'red',
-      }}>
-        {/* {props.height} */}
+        backgroundColor:
+          props.color === 0
+            ? "white"
+            : props.color === 1
+            ? "green"
+            : props.color === 3
+            ? "blue"
+            : "red",
+      }}
+    >
+      {/* {props.height} */}
     </div>
-  )
+  );
 }
 
-export default ArrayBar
+export default ArrayBar;
